8981076e4fce934090f32e57e43f531c013e0930
felix <88458605+power3lectronics@users.noreply.github.com>
2021-08-05 00:24:45 -0300

    Update ordenaCresc.py

--- ordenaCresc.py
+++ ordenaCresc.py
@@ -1,26 +1,30 @@
-list1=[1245,456033,7,1,345,15,20,0]
-list2=['a','b','c','d','e','f','g','h']
- 
-print ("Num original: "+str(list1))
-print ("Letter original: "+str(list2))
- 
-print('\n')
- 
-flag = True
- 
-while (flag):
-    flag = False
-    for i in range(len(list1)-1):
-        if (list1[i] > list1[i+1]):
-            aux1 = list1[i+1]
-            list1[i+1] = list1[i]
-            list1[i] = aux1
-            
-            aux2 =list2[i+1]
-            list2[i+1] = list2[i]
-            list2[i] = aux2
-            
-            flag = True
- 
-print ("Nums sorted: "+str(list1))
-print ("Letters sorted: "+str(list2))
+def crescSort(list1,list2):
+    print ("Num original: "+str(list1))
+    print ("Letter original: "+str(list2))
+     
+    print('\n')
+     
+    flag = True
+     
+    while (flag):
+        flag = False
+        for i in range(len(list1)-1):
+            if (list1[i] > list1[i+1]):
+                aux1 = list1[i+1]
+                list1[i+1] = list1[i]
+                list1[i] = aux1
+                
+                aux2 =list2[i+1]
+                list2[i+1] = list2[i]
+                list2[i] = aux2
+                
+                flag = True
+     
+    print ("Nums sorted: "+str(list1))
+    print ("Letters sorted: "+str(list2))
+
+
+list11=[1245,456033,7,1,345,15,20,0]
+list22=['a','b','c','d','e','f','g','h']
+
+crescSort(list11,list22)
